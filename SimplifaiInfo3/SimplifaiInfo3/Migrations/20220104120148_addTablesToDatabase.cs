09e41d1b5b9fd6a9751fe49edf5037e55a876a21
Abhishek Ingale <abhishek.ingale08@gmail.com>
2022-01-27 13:50:20 +0530

    Adding SimplifaiInfo to remote Repository

---
+++ SimplifaiInfo3/SimplifaiInfo3/Migrations/20220104120148_addTablesToDatabase.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimplifaiInfo3.Migrations
+{
+    public partial class addTablesToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductTable",
+                columns: table => new
+                {
+                    productId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    productName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    productCost = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTable", x => x.productId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClientsTable",
+                columns: table => new
+                {
+                    clientId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    clientName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    priceOfPurchase = table.Column<double>(type: "float", nullable: false),
+                    productId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientsTable", x => x.clientId);
+                    table.ForeignKey(
+                        name: "FK_ClientsTable_ProductTable_productId",
+                        column: x => x.productId,
+                        principalTable: "ProductTable",
+                        principalColumn: "productId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientsTable_productId",
+                table: "ClientsTable",
+                column: "productId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClientsTable");
+
+            migrationBuilder.DropTable(
+                name: "ProductTable");
+        }
+    }
+}
---
+++ SimplifaiInfo3/SimplifaiInfo3/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimplifaiInfo3.Models
+{
+    public class Product
+    {
+        [Key]
+        public int productId { get; set; }
+
+        [Required]
+        public string productName { get; set; }
+
+        //[Range(0, double.MaxValue, ErrorMessage = "Enter Valid Cost")]
+        public double productCost { get; set; }
+
+        public ICollection<Clients> clients { get; set; }
+
+
+    }
+}
